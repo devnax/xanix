1e76bfbde770dbf55eaf32bde2ab4051296fcb3c
Naxrul Ahmed <devnaxrul@gmail.com>
2026-08-22 18:01:49 +0600

    working

--- .xanix/client/contact.js
+++
@@ -1,8 +0,0 @@
-import { j as jsxRuntimeExports } from './jsx-runtime-BMrLYoZb.js';
-
-const Contact = () => {
-  return /* @__PURE__ */ jsxRuntimeExports.jsx("div", { children: "Contact Page" });
-};
-
-export { Contact as default };
-//# sourceMappingURL=contact.js.map
